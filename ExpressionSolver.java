e135c278027682f2a1492c1185d8dcff8d885040
XDTerminated <gupta.sayam2006@gmail.com>
2023-03-18 20:58:52 +0000

    Finished creatoing expression solver

---
+++ ExpressionSolver.java
@@ -0,0 +1,5 @@
+public class ExpressionSolver {
+    public static void main(String[] args) {
+
+    }
+}
--- Solver.java
+++ Solver.java
@@ -1,15 +1,15 @@
 public class Solver {
     public static void main(String[] args) {
-        solve("(2 + (2 + (2 + 2)) * 3)");
+        System.out.println(solve("(276 + (23 + (2 - 4)))"));
     }
 
-    public static void solve(String expression) {
+    public static String solve(String expression) {
         for (int i = 0; i < expression.length(); i++) {
             if (expression.substring(i, i + 1).equals("(")) {
                 for (int j = expression.length() - 1; j > i; j--) {
                     if (expression.charAt(j) == ')') {
-                        solve(expression.substring(i + 1, j));
-                        
+                        String s = solve(expression.substring(i + 1, j));
+                        expression = expression.substring(0, i) + s + expression.substring(j + 1, expression.length());
                         break;
                     }
                 }
@@ -18,21 +18,126 @@ public class Solver {
 
             }
         }
+        
+        return solve2(expression);
     }
 
-    public static int solve2(String first, String operator, String second) {
-        if (operator.equals("+")) {
-            return Integer.parseInt(first) + Integer.parseInt(second);
-        } else if (operator.equals("-")) {
-            return Integer.parseInt(first) - Integer.parseInt(second);
-        } else if (operator.equals("/")) {
-            return Integer.parseInt(first) / Integer.parseInt(second);
-        } else if (operator.equals("*")) {
-            return Integer.parseInt(first) * Integer.parseInt(second);
-        } else if (operator.equals("^")) {
-            return (int) Math.pow(Integer.parseInt(first), Integer.parseInt(second));
+    public static String solve2(String expression) {
+        while (expression.contains("^")) {
+            String first = ""; String second = "";
+            int i = expression.indexOf("^"); int x = i - 2; int y = i + 2;
+            while (x >= 0) {
+                if (expression.charAt(x) == ' ') {
+                    break;
+                }
+                first = expression.charAt(x) + first;
+                x--;
+            }
+
+            while (y < expression.length()) {
+                if (expression.charAt(y) == ' ') {
+                    break;
+                }
+                second = second + expression.charAt(y);
+                y++;
+            }
+
+            int z = (int) Math.pow(Integer.parseInt(first), Integer.parseInt(second));
+            expression = expression.substring(0, x + 1) + z + expression.substring(y, expression.length());
         }
 
-        return 0;
+        while (expression.contains("/")) {
+            String first = ""; String second = "";
+            int i = expression.indexOf("/"); int x = i - 2; int y = i + 2;
+            while (x >= 0) {
+                if (expression.charAt(x) == ' ') {
+                    break;
+                }
+                first = expression.charAt(x) + first;
+                x--;
+            }
+
+            while (y < expression.length()) {
+                if (expression.charAt(y) == ' ') {
+                    break;
+                }
+                second = second + expression.charAt(y);
+                y++;
+            }
+
+            int z = (int) Integer.parseInt(first) / Integer.parseInt(second);
+            expression = expression.substring(0, x + 1) + z + expression.substring(y, expression.length());
+        }
+
+        while (expression.contains("*")) {
+            String first = ""; String second = "";
+            int i = expression.indexOf("*"); int x = i - 2; int y = i + 2;
+            while (x >= 0) {
+                if (expression.charAt(x) == ' ') {
+                    break;
+                }
+                first = expression.charAt(x) + first;
+                x--;
+            }
+
+            while (y < expression.length()) {
+                if (expression.charAt(y) == ' ') {
+                    break;
+                }
+                second = second + expression.charAt(y);
+                y++;
+            }
+
+            int z = (int) Integer.parseInt(first) * Integer.parseInt(second);
+            expression = expression.substring(0, x + 1) + z + expression.substring(y, expression.length());
+        }
+
+        while (expression.contains("+")) {
+            String first = ""; String second = "";
+            int i = expression.indexOf("+"); int x = i - 2; int y = i + 2;
+            while (x >= 0) {
+                if (expression.charAt(x) == ' ') {
+                    break;
+                }
+                first = expression.charAt(x) + first;
+                x--;
+            }
+
+            while (y < expression.length()) {
+                if (expression.charAt(y) == ' ') {
+                    break;
+                }
+                second = second + expression.charAt(y);
+                y++;
+            }
+
+            int z = (int) Integer.parseInt(first) + Integer.parseInt(second);
+            expression = expression.substring(0, x + 1) + z + expression.substring(y, expression.length());
+        }
+
+        while (expression.contains("-")) {
+            String first = ""; String second = "";
+            int i = expression.indexOf("-"); int x = i - 2; int y = i + 2;
+            while (x >= 0) {
+                if (expression.charAt(x) == ' ') {
+                    break;
+                }
+                first = expression.charAt(x) + first;
+                x--;
+            }
+
+            while (y < expression.length()) {
+                if (expression.charAt(y) == ' ') {
+                    break;
+                }
+                second = second + expression.charAt(y);
+                y++;
+            }
+
+            int z = Integer.parseInt(first) - Integer.parseInt(second);
+            expression = expression.substring(0, x + 1) + z + expression.substring(y, expression.length());
+        }
+
+        return expression;
     }
 }
